7866cc291bb7ade7ae50c076b899f4b57fe42cad
CD-prog <61663007+CD-prog@users.noreply.github.com>
2020-07-31 09:21:31 -0500

    updated

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require("express")
-const exphbs = require("express-handlebars")
+const express = require('express')
+const exphbs = require('express-handlebars')
 
 const app = express()
 const PORT = 8080
@@ -8,12 +8,27 @@ app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
 var icecreams = [
-    {name: 'vanilla', price: 10, awesomeness: 3},
-    {name: 'chocolate', price: 4, awesomeness: 8},
-    {name: 'banana', price: 1, awesomeness: 1},
-    {name: 'greentea', price: 5, awesomeness: 7},
-    {name: 'jawbreakers', price: 6, awesomeness: 2},
-    { name: "pistachio", price: 11, awesomeness: 15 }
-  ];
+  {name: 'vanilla', price: 10, awesomeness: 3},
+  {name: 'chocolate', price: 4, awesomeness: 8},
+  {name: 'banana', price: 1, awesomeness: 1},
+  {name: 'greentea', price: 5, awesomeness: 7},
+  {name: 'jawbreakers', price: 6, awesomeness: 2},
+  {name: "pistachio", price: 11, awesomeness: 15 }
+];
 
-app.listen(PORT, () => console.log(`on http://localhost:${PORT}`));
+app.get(`/`, (req, res) => {
+  res.render('index')
+})
+
+app.get(`/icecreams/:name`, (req, res) => {
+  const targetFlavor = req.params.name
+  const found = icecreams.find(flavor => flavor.name === targetFlavor)
+  res.render('flavor', found)
+})
+
+app.get(`/icecreams`, (req, res) => {
+  // loop over all the ice creams and display them all to the user.
+  res.render('flavors', { icecreams })
+})
+
+app.listen(PORT, () => console.log(`🚀 on http://localhost:${PORT}`))
